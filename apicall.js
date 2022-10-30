d96cb35e84d288312d31318ee167683f4d669ac0
JuSTC3 <73889801+JuSTC3@users.noreply.github.com>
2022-10-30 10:15:10 +0530

    Create apicall.js
    sample node.js api call using fetch

---
+++ apicall.js
@@ -0,0 +1,13 @@
+const userAction = async () => {
+    const response = await fetch('http://example.com/movies.json', {
+      method: 'POST',
+      body: myBody, // string or object
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+  }
+
+  userAction();
