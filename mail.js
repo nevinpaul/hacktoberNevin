cb44558bd3f6abf59731db400fce817805c18e67
JuSTC3 <73889801+JuSTC3@users.noreply.github.com>
2022-10-30 10:13:13 +0530

    Create mail.js
    nodemail sample code

---
+++ mail.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer')
+const config = require("./config")
+
+const transporter = nodemailer.createTransport({
+    service: config.mail.service,
+    auth: {
+        user: config.mail.user,
+        pass: config.mail.pass
+    }
+})
+
+const sendMail = function (mailOptions) {
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error)
+        } else {
+          console.log('Email sent: ' + info.response + ' => ' + mailOptions.to)
+        }
+      })
+}
+
+module.exports = {
+    sendMail
+}
